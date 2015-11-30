db7b974622a68e72aa5b403a63634aca4fe7c522
takayukioda <takayuki@odashi.me>
2015-11-30 23:02:25 +0900

    Validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,12 @@ class UserController extends Controller {
 	 * Create new user by given parameters
 	 */
 	public function join (Request $request) {
-		return response()->json($request->input());
+		$validator = Validator::make($request->all(), [
+			'name'     => 'required|max:32',
+			'email'    => 'required|email',
+			'password' => 'required|max:255',
+		]);
+		// $newuser = new User;
+		return response()->json($validator->fails());
 	}
 }
