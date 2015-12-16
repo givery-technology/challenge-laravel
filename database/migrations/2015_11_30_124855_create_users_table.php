7430a35194b7af899c48a35543a41f7956b6457b
takayukioda <takayuki@odashi.me>
2015-12-16 21:09:10 +0900

    Update schema; name -> username

--- database/migrations/2015_11_30_124855_create_users_table.php
+++ database/migrations/2015_11_30_124855_create_users_table.php
@@ -14,8 +14,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email');
-            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
             $table->string('password');
             $table->date('birthday')->nullable();
             $table->string('company')->nullable();
